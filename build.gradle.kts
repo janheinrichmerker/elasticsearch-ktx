ea4a310be67fa937bdcd603b51a1f120f7bd6afe
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-01-15 13:27:23 +0100

    Add Gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.11"
+}
+
+group = "com.heinrichreimer"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+//    compile("org.elasticsearch.client:transport:6.5.2")
+    compile("org.elasticsearch.client:elasticsearch-rest-high-level-client:6.5.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
